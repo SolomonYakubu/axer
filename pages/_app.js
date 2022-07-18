91e851dd8235accd1e2dc899bc3ec37941d0f5b7
SolomonYakubu <soloyaks.sy@gmail.com>
2022-07-18 23:26:46 +0100

    Added next seo

--- pages/_app.js
+++ pages/_app.js
@@ -2,6 +2,7 @@ import "../styles/globals.css";
 import { SessionProvider } from "next-auth/react";
 import Head from "next/head";
 import Header from "../components/Header";
+import { NextSeo } from "next-seo";
 function MyApp({ Component, pageProps }) {
   return (
     <>
@@ -10,6 +11,33 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <meta name="theme-color" content="rgb(107, 36, 168)" />
       </Head>
+      <NextSeo
+        title="Axer"
+        titleTemplate="Axer"
+        defaultTitle="Axer"
+        description="Axer is a Link Shortener that gives you the power to grow and monitor your brand."
+        canonical="https://axer.ga"
+        openGraph={{
+          url: "https://axer.ga",
+          title: "Axer",
+          description:
+            "Axer is a Link Shortener that gives you the power to grow and monitor your brand",
+          // images: [
+          //   {
+          //     url: "/og-image.png",
+          //     width: 800,
+          //     height: 420,
+          //     alt: "Axer",
+          //   },
+          // ],
+        }}
+        twitter={{
+          handle: "@King__Solo",
+          site: "@King__Solo",
+          cardType: "summary_large_image",
+        }}
+      />
+
       <SessionProvider session={pageProps.session}>
         <Header />
         <Component {...pageProps} />
