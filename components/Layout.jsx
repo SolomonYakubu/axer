fadb6272bcc046de5f14c9a356ebe3ffe343603e
SolomonYakubu <soloyaks.sy@gmail.com>
2022-07-20 01:18:17 +0100

    Integrated layout

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export default function Layout({ children }) {
   return (
-    <div className="min-h-[calc(100vh-64px)] h-[60vh] p-0 m-0 mt-16">
+    <div className="min-h-[calc(100vh-64px)] h-[20vh] p-0 m-0 mt-16">
       {children}
     </div>
   );
