4ccf66affa0d37441ac00762b9eb742980a29c3d
SolomonYakubu <soloyaks.sy@gmail.com>
2022-07-21 14:28:50 +0100

    Updated security

--- pages/api/url.js
+++ pages/api/url.js
@@ -6,44 +6,42 @@ import { authOptions } from "./auth/[...nextauth]";
 import crypto from "crypto";
 
 export default async function handler(req, res) {
-  const session = await unstable_getServerSession(req, res, authOptions);
-  console.log(session);
-  if (session) {
-    switch (req.method) {
-      case "POST":
-        const { fullUrl, customPhrase, user } = req.body;
-        if (
-          !fullUrl.match(
-            new RegExp(
-              /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi
-            )
+  switch (req.method) {
+    case "POST":
+      // const session = await unstable_getServerSession(req, res, authOptions);
+      // if (session) {
+      const { fullUrl, customPhrase, user } = req.body;
+      if (
+        !fullUrl.match(
+          new RegExp(
+            /[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_\+.~#?&//=]*)?/gi
           )
-        ) {
-          return res.status(400).json("error");
-        }
-        const formatedPhrase =
-          (customPhrase && customPhrase.split(" ").join("-")) ||
-          crypto.randomBytes(5).toString("hex");
-        try {
-          await connectMongo();
-          let host = req.headers.host;
-          const url = await new Url({
-            fullUrl,
-            shortUrl: `${formatedPhrase}`,
-            user,
-          }).save();
-          res.status(201).json({
-            fullUrl,
-            shortUrl: `${host}/${formatedPhrase}`,
-            clicks: 0,
-          });
-        } catch (error) {
-          res.json(error.message);
-        }
-
-        break;
-      case "GET":
-    }
+        )
+      ) {
+        return res.status(400).json("error");
+      }
+      const formatedPhrase =
+        (customPhrase && customPhrase.split(" ").join("-")) ||
+        crypto.randomBytes(5).toString("hex");
+      try {
+        await connectMongo();
+        let host = req.headers.host;
+        const url = await new Url({
+          fullUrl,
+          shortUrl: `${formatedPhrase}`,
+          user,
+        }).save();
+        res.status(201).json({
+          fullUrl,
+          shortUrl: `${host}/${formatedPhrase}`,
+          clicks: 0,
+        });
+      } catch (error) {
+        res.json(error.message);
+      }
+      // }
+      // return res.status(401).json();
+      break;
+    case "GET":
   }
-  return res.status(401).json();
 }
